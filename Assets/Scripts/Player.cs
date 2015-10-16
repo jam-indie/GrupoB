1b148f44330fbb3d926ea375f00ef168921cd71a
AndreFelipe1997 <Andrefelipe1997@yahoo.com.br>
2015-10-15 23:42:36 -0300

    As pastas antigas foram substituidas por novas pastas, melhor organizadas. Novas scene adicionada. Script Movement.cs foi substituido por Player.cs. Novo Scripts: TangiblePlataform.cs, que faz com que o player possa passar por baixo das plataformas e não atravessa-las ao cair em cima delas.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour 
+{
+	//ControllKeys
+	public KeyCode Space;
+	public KeyCode RightArrow;
+	public KeyCode LeftArrow;
+
+	//Physics
+	private Rigidbody2D MyRigidBody;
+	private int Clicks;
+	public bool CanJump;
+	public float JumpForce;
+	public Animator Anim;
+	public SpriteRenderer MySpriteRenderer;
+	public Sprite MySprite;
+
+	void Start () 
+	{
+		MyRigidBody = GetComponent<Rigidbody2D>();
+		Anim = GetComponent<Animator> ();
+		MySpriteRenderer = GetComponent<SpriteRenderer> ();
+		//CanJump = true;
+		Anim.enabled = false;
+	}
+
+	void Update () 
+	{
+		Controls();
+	}
+
+	void Controls ()
+	{
+		if (Input.GetKey(RightArrow)) 
+		{
+			transform.position += new Vector3(5*Time.deltaTime,0,0);
+			gameObject.transform.localScale = new Vector3(1, 1,1);
+			Anim.enabled = true;
+		} else
+		{
+			MySpriteRenderer.sprite = MySprite;
+			Anim.enabled = false;
+		}
+
+		if (Input.GetKey(LeftArrow))
+		{
+			transform.position -= new Vector3(5*Time.deltaTime,0,0);
+			gameObject.transform.localScale = new Vector3(-1, 1,1);
+			Anim.enabled = true;
+		}
+		if (Input.GetKeyDown(Space) && CanJump == true) 
+		{
+			MyRigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+			CanJump = false;
+		}
+	}
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground") 
+		{
+			CanJump = true;
+			Debug.Log ("Enter");
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D coll)
+	{
+		if(coll.gameObject.tag == "Ground")
+		{
+			CanJump = false;
+			Debug.Log ("Exit");
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag == "Ground")
+		{
+			CanJump = true;
+			Debug.Log ("Stay");
+		}
+	}
+}
---
+++ Assets/Scripts/TangiblePlataform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TangiblePlataform : MonoBehaviour 
+{
+	public bool IsTangible;
+	public Collider2D MyCollider;
+	public GameObject Player;
+	
+	void Start () 
+	{
+		MyCollider = gameObject.GetComponent<Collider2D> ();
+		Player = GameObject.Find("Player");
+	}
+
+	void Update () 
+	{
+		if(IsTangible)
+		{
+			if(Player.transform.position.y < this.gameObject.transform.position.y)
+			{
+				this.MyCollider.isTrigger = true;
+			}
+
+			if(Player.transform.position.y > this.gameObject.transform.position.y + this.gameObject.transform.localScale.y)
+			{
+				this.MyCollider.isTrigger = false;
+			}
+		}
+	}
+}
