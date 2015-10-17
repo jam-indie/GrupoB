ac908b972c95a32fac094cfb2d68865cfa9b2f60
AndreFelipe1997 <Andrefelipe1997@yahoo.com>
2015-10-17 00:45:46 -0300

    Criando o script de correção de tela!
    Está 100% funcional, e semicomentado.
    
    Vou comentar ele agora.
    
    commitando a scene alterada com o script de correção de camera.

--- Assets/Scripts/SceneScripts/CameraCorrection.cs
+++ Assets/Scripts/SceneScripts/CameraCorrection.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraCorrection : MonoBehaviour {
+public class CameraCorrection : MonoBehaviour 
+{
+	public Camera Main;
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject Player;
+
+	public float CameraCorrectionX;
+	public float CameraCorrectionY;
+
+	public float FollowSpeed;
+
+	public float LimitX;
+	public float LimitY;
+
+	public bool Follow;
+	public bool AutomaticLimiter;//If true, the script get's the start position of player and equal the Limit X to him position X and the LimitY to him position Y.
+
+	void Start () 
+	{
+		Main = Camera.main;
+		Player = GameObject.Find ("Player");
+
+		if(AutomaticLimiter)
+		{
+			LimitX = Player.transform.position.x;
+			LimitY = Player.transform.position.y;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () 
+	{
+		if(Player.transform.position.x <= LimitX || Player.transform.position.x >= -LimitX)
+		{
+			Main.transform.position =  Vector3.Lerp(Main.transform.position, new Vector3(Main.transform.position.x, Player.transform.position.y + CameraCorrectionY, Main.transform.position.z), FollowSpeed);
+		}
+		else if(Player.transform.position.y <= LimitY || Player.transform.position.y >= -LimitY - CameraCorrectionY)
+		{
+			Main.transform.position =  Vector3.Lerp(Main.transform.position, new Vector3(Player.transform.position.x + CameraCorrectionX, Main.transform.position.y, Main.transform.position.z), FollowSpeed);
+		}
+		else
+		{
+			Main.transform.position =  Vector3.Lerp(Main.transform.position, new Vector3(Player.transform.position.x + CameraCorrectionX, Player.transform.position.y + CameraCorrectionY, Main.transform.position.z), FollowSpeed);
+		}
 	}
 }
