5e378c3b1bba66463a11a91362a8b732c63f48f5
YaneBelo <yanesrs15@gmail.com>
2015-10-17 16:09:41 -0300

    Criada a cena HUDscene, e os scripts Score e HUDVida...

---
+++ Assets/Assets/HudScript/HudVida.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudVida : MonoBehaviour {
+
+    public int vida = 3; // A vida iniciara com um valor
+    public GUIText textoVida;  // Poderia ser GUITexture
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        textoVida.text = "Vida: " + vida; //Texto que irá aparecer na GUIText
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vida <= 0) 
+        {
+            //Se a vida for menor ou igual a zero, vai reiniciar o jogo "Coloquei assim, porque ainda não tem Game Over"
+            // E tem que criar um OnCollider2D pra quando o objeto colidir com inimigo por exemplo ir perdendo a vida e morrer.
+
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
---
+++ Assets/Assets/HudScript/Score.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+
+    public GUIText textoScore;
+    public int score;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        score = 0; // Será iniciado com o valor zero.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textoScore.text = "Score: " + score;  //O nome que irá aparecer na GUItex, mais o valor.
+
+
+    }
+   /* void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "cubo")
+        {
+
+            other.gameObject.SetActive(false); // Quando o player entrar em contato com o objeto, somara um ponto e esse objeto fica desativado.
+
+            score = score + 1;
+
+        }
+    }*/
+}
