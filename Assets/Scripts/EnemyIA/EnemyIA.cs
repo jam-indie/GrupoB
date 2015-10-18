7ee4910b46e0fe4ed8fe1f03e8b589650f27407a
Matheus123456 <matheus_onlive@live.com>
2015-10-17 21:52:16 -0300

    Soares: Iniciei testes da inteligencia artificial. O inimigo ta perseguindo mas nao pula nas plataformas.

---
+++ Assets/Scripts/EnemyIA/EnemyIA.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyIA : MonoBehaviour 
+{
+	public Transform player;
+	private float vel;
+	// Use this for initialization
+	void Start () 
+	{
+		vel = 0.1f;	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Vector3.Distance (player.position, transform.position) < 15) 
+		{
+			transform.position = Vector3.Slerp(transform.position, player.transform.position, vel*Time.deltaTime);
+		}
+		if (Vector3.Distance (player.position, transform.position) < 5) 
+		{
+			vel = 0;
+		}
+		if (Vector3.Distance (player.position, transform.position) > 5) 
+		{
+			vel = 0.1f;
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyIA/PlayerTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTest : MonoBehaviour 
+{
+	public Rigidbody pular;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pular = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(4*Time.deltaTime,0,0);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position -= new Vector3(4*Time.deltaTime,0,0);
+		} 
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			pular.AddForce(0,16,0, ForceMode.Impulse);
+		}
+	}
+}
