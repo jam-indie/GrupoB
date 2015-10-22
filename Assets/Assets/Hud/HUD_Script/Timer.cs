af9405d08e2af5b09abd9675b17c79326afb4d74
YaneBelo <yanesrs15@gmail.com>
2015-10-22 01:36:11 -0200

    Foi colocada a pasta Hud_script com os scripts( vida e timer) dentro da pasta Hud.

---
+++ Assets/Assets/Hud/HUD_Script/Timer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public float tempo = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void OnGUI()
+    {
+
+        GUI.Box(new Rect(170, 30, 200, 50), "" + tempo.ToString("0"));
+
+
+
+    }
+    void Update()
+    {
+
+        tempo -= Time.deltaTime;
+
+        if (tempo <= 0)
+        {
+
+            tempo = 0;
+
+            Application.LoadLevel("GameOver");
+
+
+        }
+
+    }
+}
